be0fce4ba0c769b7b71dab507023f958591a8eb5
iLay1678 <807190135@qq.com>
2020-03-27 09:32:27 +0800

    Update Valine.Ex.js

--- js/Valine.Ex.js
+++ js/Valine.Ex.js
@@ -1825,7 +1825,7 @@
 				}
 			},
 			x = function() {
-				$.getJSON("https://api.ip.sb/jsonip", function(e) {
+				$.getJSON("https://api.ipify.org/?format=json", function(e) {
 					u.ip = e.ip
 				})
 			};
